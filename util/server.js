cc7cc1e084cc990c41b892b01d1edc34832f8011
()gres <serdarsaryjayev2002@gmail.com>
2023-04-15 17:15:04 +0500

    Main script

--- util/server.js
+++ util/server.js
@@ -3,8 +3,16 @@ const app = express()
 require('dotenv').config()
 const colors = require('colors')
 app.use(express.json())
+const cors = require('cors')
+
+// Cors
+const corsOptions = {
+    origin: '*',
+}
+app.use(cors(corsOptions))
 
 // Routes
+const Admin=require('../routers/admin/admin.router.js')
 const AdminUsers = require('../routers/admin/users.router.js')
 const User = require('../routers/client/users.router.js')
 const AdminCategory = require('../routers/admin/category.router.js')
@@ -15,6 +23,7 @@ const AdminLike = require('../routers/admin/like.router.js')
 app.use('/public', express.static('./public'))
 
 // Admin routes
+app.use('/admin/admin',Admin)
 app.use('/admin/user', AdminUsers)
 app.use('/admin/category', AdminCategory)
 app.use('/admin/like', AdminLike)
