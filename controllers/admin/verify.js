5f64b05ed68eb3d24c6a1999af0e5f5a1c5b2c95
()gres <serdarsaryjayev2002@gmail.com>
2023-04-15 17:17:32 +0500

    Admin verify

---
+++ controllers/admin/verify.js
@@ -0,0 +1,46 @@
+const models = require('../../models')
+const jwt = require('jsonwebtoken')
+
+module.exports = async (req, res, next) => {
+    if (!!req.headers.authorization) {
+        if (req.headers.authorization.split(' ')[0] === 'Bearer') {
+            const token = req.headers.authorization.split(' ')[1]
+            const { user_id, type } = jwt.verify(token, process.env.TOKEN_KEY)
+            if (parseInt(type) < 3) {
+                res.status(502).json({
+                    msg: "You are not admin"
+                })
+            } else {
+                let user = await models.Users.findOne({
+                    where: {
+                        id: user_id
+                    }
+                })
+                if (!user) {
+                    res.status(503).json({
+                        msg: 'You user undefine'
+                    })
+                } else if (user.ban) {
+                    res.status(403).json({
+                        msg: 'You are baned'
+                    })
+                } else if (user.type < 3) {
+                    res.status(503).json({ msg: 'You are not admin' })
+                } else {
+                    req.id = user_id
+                    req.type = type
+                    next()
+                }
+
+            }
+        } else {
+            res.status(502).json({
+                msg: "You are not admin"
+            })
+        }
+    } else {
+        res.status(400).json({
+            msg: "Token is not"
+        })
+    }
+}
